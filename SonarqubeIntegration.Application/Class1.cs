d23b2141c137ea1d61ee83e260495fa4a9b262fa
VenkataSiva Reddy Padala <siva@logics.com>
2024-02-15 20:49:02 +0530

    Updated

--- SonarqubeIntegration.Application/Class1.cs
+++ SonarqubeIntegration.Application/Class1.cs
@@ -2,6 +2,21 @@
 {
     public class Class1
     {
+        public int UserId { get; set; }
 
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Email2 { get; set; }
+
+        public string Email3 { get; set; }
+
+        public string Email4 { get; set; }
+
+
+        public string Email5 { get; set; }
     }
 }
